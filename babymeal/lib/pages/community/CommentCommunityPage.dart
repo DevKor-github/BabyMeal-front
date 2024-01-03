334830301d72d2ddd8169c663de0d18dbe3c7b1a
jjjh02 <jinnyshur@korea.ac.kr>
2024-01-04 02:50:24 +0900

    commentcommunitypage 수정

--- babymeal/lib/pages/community/CommentCommunityPage.dart
+++ babymeal/lib/pages/community/CommentCommunityPage.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
 
-class AlarmCommunityPage extends StatefulWidget {
-  const AlarmCommunityPage({super.key});
+class CommentCommunityPage extends StatefulWidget {
+  const CommentCommunityPage({super.key});
 
   @override
-  State<AlarmCommunityPage> createState() => _AlarmCommunityPageState();
+  State<CommentCommunityPage> createState() => _CommentCommunityPageState();
 }
 
-class _AlarmCommunityPageState extends State<AlarmCommunityPage> {
-  List<String> Alarm_image = [
-    'assets/images/notice.png',
-    'assets/images/comment.png',
-    'assets/images/like.png'
+class _CommentCommunityPageState extends State<CommentCommunityPage> {
+  TextEditingController commentController = TextEditingController();
+  List<String> CommentID = ['서준엄마', '준서아빠22', '육아고수', '육아초보'];
+  List<String> CommentContext = [
+    '유용한 정보 감사합니다. 다음번에는 꼭 알려주신 방법대로 해봐야겠어요~~',
+    '잘 보고 갑니다.',
+    '여기에 이걸 추가하면 더 좋을 것 같아요.',
+    '-'
   ];
-
-  List<String> Alarm_Context = [
-    '[공지] 2023.11.20부터 OOO 서비스가 개편됩니다. 0시부터 16시까지는 사용이 제한됩니다.',
-    '유아식 식단표 | 밥솥 유아식 레시피 공유합니다. 머머머부터 머머머머머까지 글에 댓글을 달았습니다.',
-    'ooo님이 아이훈육법에 대한 내 생각 글에 찜을 눌렀습니다.'
-  ];
-
-  List<String> Alarm_Time = ['12시간전', '10일전', '15일전'];
+  List<String> CommentTime = ['2023.11.12', '1시간 전', '20분 전', '-'];
+  ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: Color(0xFFF4F3F0),
         appBar: AppBar(
           title: Text(
-            '\n알림',
+            '\n댓글',
             style: TextStyle(
               fontSize: 18.0,
               fontWeight: FontWeight.bold,
@@ -54,11 +52,11 @@ class _AlarmCommunityPageState extends State<AlarmCommunityPage> {
           centerTitle: true,
           elevation: 0,
         ),
-        body: Alarm_Context.isEmpty
+        body: CommentContext.isEmpty
             ? Center(
                 child: RichText(
                   text: TextSpan(
-                      text: '알림이 없습니다.',
+                      text: '작성된 댓글이 없습니다.',
                       style: TextStyle(
                         color: Color(0xFF9E9E9E),
                         fontSize: 17,
@@ -69,59 +67,169 @@ class _AlarmCommunityPageState extends State<AlarmCommunityPage> {
                       )),
                 ),
               )
-            : ListView.builder(
-                shrinkWrap: true,
-                itemCount: Alarm_Context.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF4F3F0),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 20, 0, 0),
-                              child: Image.asset(Alarm_image[index]),
-                            ),
-                            Container(
-                                width: 356,
-                                padding: EdgeInsets.fromLTRB(15, 20, 20, 0),
-                                child: RichText(
-                                    textAlign: TextAlign.left,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 4,
-                                    text: TextSpan(
-                                        text: Alarm_Context[index],
-                                        style: TextStyle(
-                                          color: Color(0xFF212121),
-                                          fontSize: 16,
-                                          fontFamily: 'Pretendard',
-                                          fontWeight: FontWeight.w600,
-                                          height: 0,
-                                          letterSpacing: -0.50,
-                                        )))),
-                          ],
+            : Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      reverse: false,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxHeight: MediaQuery.of(context).size.height -
+                              130, // 원하는 최대 높이로 설정
                         ),
-                        Padding(
-                            padding: EdgeInsets.fromLTRB(71, 10, 0, 20),
-                            child: RichText(
-                                textAlign: TextAlign.left,
-                                text: TextSpan(
-                                    text: Alarm_Time[index],
-                                    style: TextStyle(
-                                      color: Color(0xFF616161),
-                                      fontSize: 12,
-                                      fontFamily: 'Pretendard',
-                                      fontWeight: FontWeight.w500,
-                                      height: 0,
-                                      letterSpacing: -0.50,
-                                    )))),
-                      ],
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: CommentID.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            // 리스트 아이템 생성
+                            return Container(
+                              child: Container(
+                                  child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Padding(
+                                                  padding: EdgeInsets.fromLTRB(
+                                                      20, 20, 10, 0),
+                                                  child: RichText(
+                                                      textAlign: TextAlign.left,
+                                                      text: TextSpan(
+                                                          text: CommentID[
+                                                              index], //id
+                                                          style: TextStyle(
+                                                            color: Color(
+                                                                0xFF212121),
+                                                            fontSize: 16,
+                                                            fontFamily:
+                                                                'Pretendard',
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                            height: 0,
+                                                            letterSpacing:
+                                                                -0.50,
+                                                          )))),
+                                              Padding(
+                                                  padding: EdgeInsets.fromLTRB(
+                                                      20, 20, 10, 0),
+                                                  child: RichText(
+                                                      textAlign: TextAlign.left,
+                                                      text: TextSpan(
+                                                          text: CommentTime[
+                                                              index], //date
+                                                          style: TextStyle(
+                                                            color: Color(
+                                                                0xFF616161),
+                                                            fontSize: 12,
+                                                            fontFamily:
+                                                                'Pretendard',
+                                                            fontWeight:
+                                                                FontWeight.w500,
+                                                            height: 0,
+                                                            letterSpacing:
+                                                                -0.50,
+                                                          )))),
+                                            ]),
+                                        Container(
+                                            width: 430,
+                                            padding: EdgeInsets.fromLTRB(
+                                                20, 10, 20, 20),
+                                            child: RichText(
+                                                textAlign: TextAlign.left,
+                                                overflow: TextOverflow.ellipsis,
+                                                maxLines: 3,
+                                                text: TextSpan(
+                                                    text: //comment
+                                                        CommentContext[index],
+                                                    style: TextStyle(
+                                                      color: Color(0xFF212121),
+                                                      fontSize: 16,
+                                                      fontFamily: 'Pretendard',
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                      height: 0,
+                                                      letterSpacing: -0.50,
+                                                    )))),
+                                      ]),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFF4F3F0),
+                                  )),
+                            );
+                          },
+                        ),
+                      ),
                     ),
-                  );
-                }));
+                  ),
+                  Container(
+                      color: Color(0xFFFFFFFF),
+                      child: Row(children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: commentController,
+                            decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFFE0E0E0),
+                                  width: 1,
+                                ),
+                              ),
+                              labelStyle: TextStyle(
+                                color: Color(0xFFE0E0E0),
+                                fontSize: 18,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                                letterSpacing: -0.36,
+                              ),
+                              hintText: '댓글을 입력해주세요.',
+                              hintStyle: TextStyle(
+                                color: Color(0xFF9E9E9E),
+                                fontSize: 18,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                                letterSpacing: -0.36,
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0xFFFF5C39),
+                                  width: 1,
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                            maxLines: null,
+                            keyboardType: TextInputType.emailAddress,
+                            cursorColor: Color(0xFF6F61EF),
+                          ),
+                        ),
+                        TextButton(
+                          child: Text('등록',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFF757575))),
+                          onPressed: () {
+                            addComment();
+                          },
+                        ),
+                      ])),
+                ],
+              ));
+  }
+
+  void addComment() {
+    String newComment = commentController.text;
+    if (newComment.isNotEmpty) {
+      setState(() {
+        CommentContext.add(newComment);
+        commentController.clear();
+      });
+    }
   }
 }
